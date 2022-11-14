672476c80c27f0c3fe46630bd6c0371c9ebf0ca3
이주원 <60950583+Su-per@users.noreply.github.com>
2022-11-14 23:24:56 +0900

    Create clip.py

---
+++ models/clip.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+from sqlalchemy.dialects.mysql import TINYTEXT
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+metadata = Base.metadata
+
+
+class Clip(Base):
+    __tablename__ = 'clip'
+    __table_args__ = {'comment': '클립 테이블'}
+
+    id = Column(Integer, primary_key=True, comment='아이디')
+    title = Column(String(45), nullable=False, comment='제목')
+    streamer_name = Column(String(45), nullable=False, comment='스트리머 이름')
+    description = Column(TINYTEXT, nullable=False, comment='설명')
+    upload_date = Column(DateTime, nullable=False, comment='업로드 날짜')
+    views = Column(Integer, nullable=False, comment='조회수')
+    uploader_id = Column(ForeignKey('user.id', ondelete='RESTRICT', onupdate='RESTRICT'), nullable=False, index=True, comment='업로더 아이디')
+
+    uploader = relationship('User')
