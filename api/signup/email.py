1445122e25f88ed9024b054065e6b73514a17d0b
이주원 <juwoon7163@gmail.com>
2022-12-22 11:41:01 +0900

    이메일 템플릿 추가

--- api/signup/email.py
+++ api/signup/email.py
@@ -7,11 +7,13 @@ import string
 import redis
 
 
-def send_mail(email: str):
+def send_mail(email: str, code: str):
     smtp = smtplib.SMTP("smtp.gmail.com", 587)
     smtp.starttls()
     smtp.login("juwoon7163@gmail.com", "qghsykvilnjetxyz")
-    msg = MIMEText("본문")
+    with open("email_template/template.html", "r") as f:
+        content = f.read().replace("{code}", code)
+    msg = MIMEText(content, "html")
     msg["Subject"] = "Daily In GSM 인증메일"
     smtp.sendmail("juwoon7163@gmail.com", email, msg.as_string())
     smtp.quit()
@@ -27,7 +29,7 @@ def email(req: SendMail, background_tasks: BackgroundTasks):
     with redis.StrictRedis(host="127.0.0.1", port=6379, db=0) as conn:
         if conn.get(req.email) != None:
             raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="진행중인 인증이 있습니다.")
-        conn.set(req.email, code, ex=300)
+        conn.set(req.email, code, ex=10)
 
-    background_tasks.add_task(send_mail, req.email)
-    return {"message": "success" + code}
+    background_tasks.add_task(send_mail, req.email, code)
+    return {"message": "success"}
