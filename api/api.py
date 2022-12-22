febde5019d90c0ab48d726759d700fb38ba0df80
이주원 <juwoon7163@gmail.com>
2022-12-22 20:56:00 +0900

    .

--- api/api.py
+++ api/api.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .signup.account_api import signup_router
+from .signup.account_api import router as signup_router
 
 api_router = APIRouter()
 api_router.include_router(signup_router, prefix="/signup")
