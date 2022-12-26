98319a15098b73635a0316a2a05c2a528acdc17b
이주원 <juwoon7163@gmail.com>
2022-12-26 14:02:06 +0900

    api docs tag 추가

--- api/api.py
+++ api/api.py
@@ -3,5 +3,5 @@ from .signup.signup_api import router as signup_router
 from .signin.signin_api import router as signin_router
 
 api_router = APIRouter()
-api_router.include_router(signup_router, prefix="/signup")
-api_router.include_router(signin_router, prefix="/signin")
+api_router.include_router(signup_router, prefix="/signup", tags=["Signup"])
+api_router.include_router(signin_router, prefix="/signin", tags=["Signin"])
