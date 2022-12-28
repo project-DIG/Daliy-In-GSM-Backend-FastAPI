71d39f17d066d0ea4d5f256cec85679e10d7b970
이주원 <juwoon7163@gmail.com>
2022-12-28 10:35:50 +0900

    CORS 미들웨어 설정

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.api import api_router
 
 
 app = FastAPI(title="DIG")
 app.include_router(api_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     import uvicorn
