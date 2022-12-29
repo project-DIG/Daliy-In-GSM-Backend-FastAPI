7804ce744816c5077597218a9c0d78fd66a7aad9
이주원 <juwoon7163@gmail.com>
2022-12-29 11:42:19 +0900

    update 이메일 인증 시간 변수로 설정

--- api/signup/email_check.py
+++ api/signup/email_check.py
@@ -12,7 +12,7 @@ def email_check(req: CheckCode, redis_db: StrictRedis = Depends(get_redis_db)):
     if redis_db.get(req.email) == None:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="이 이메일로 진행중인 인증이 없습니다.")
     if redis_db.get(req.email).decode() == req.code:
-        redis_db.set(req.email, "success")
+        redis_db.set(req.email, "success", ex=settings.EMAIL_AUTH_EXPIRE)
         return {"detail": "성공하였습니다."}
     else:
         raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="인증번호가 다릅니다.")
