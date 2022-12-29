ff8d17290c3d130727d49c580b4577a0fd93b459
이주원 <juwoon7163@gmail.com>
2022-12-29 14:48:18 +0900

    db session 예외처리

--- db/session.py
+++ db/session.py
@@ -7,17 +7,22 @@ engine = create_engine(f"mysql+pymysql://{settings.DB_URL}")
 
 
 def get_db():
-    session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
+
     try:
+        session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
         yield session
         session.commit()
+    except Exception as e:
+        print(e)
     finally:
         session.close()
 
 
 def get_redis_db():
-    conn = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
     try:
+        conn = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
         yield conn
+    except Exception as e:
+        print(e)
     finally:
         conn.close()
