3d4f21ef03753979f0cc7903904f0ea86451c689
이주원 <juwoon7163@gmail.com>
2022-12-30 13:42:26 +0900

    .

--- main.py
+++ main.py
@@ -15,25 +15,6 @@ app.add_middleware(
 )
 
 
-@app.get("/zz")
-def zz():
-    import redis
-    from core.config import settings
-
-    r = redis.StrictRedis(host=settings.REDIS_HOST)
-    r.set("asd", "성공!!")
-    return {"asd", r.get("asd").decode()}
-
-
-@app.get("/zzzz")
-def zzzz():
-    import redis
-    from core.config import settings
-
-    r = redis.StrictRedis(host=settings.REDIS_HOST)
-    return {"asd", r.get("asd").decode()}
-
-
 if __name__ == "__main__":
     import uvicorn
 
