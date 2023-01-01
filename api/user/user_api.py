59cf27c5fcaaca9ee7ba6a0d0714717a2e894dd9
이주원 <juwoon7163@gmail.com>
2023-01-01 23:21:28 +0900

    액세스토큰으로 유저 정보받는 api

--- api/user/user_api.py
+++ api/user/user_api.py
@@ -5,6 +5,7 @@ from .like_video import router as like_video_router
 from .dislike_video import router as dislike_video_router
 from .follow import router as follow_router
 from .unfollow import router as unfollow_router
+from .mini import router as mini_router
 
 router = APIRouter()
 router.include_router(userpage_router)
@@ -13,3 +14,4 @@ router.include_router(like_video_router)
 router.include_router(dislike_video_router)
 router.include_router(follow_router)
 router.include_router(unfollow_router)
+router.include_router(mini_router)
