effc9fad35120d651067d282257fe5ca2a50f95e
이주원 <juwoon7163@gmail.com>
2023-03-06 11:04:52 +0900

    관련 환경변수 통합

--- core/config.py
+++ core/config.py
@@ -6,15 +6,9 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    DB_HOST: str = os.getenv("DB_HOST")
-    DB_PORT: str = os.getenv("DB_PORT")
-    DB_USER: str = os.getenv("DB_USER")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD")
-    DB_NAME: str = os.getenv("DB_NAME")
+    DB_URL: str = os.getenv("DB_URL")
 
-    DB_URL: str = f"{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
-    REDIS_HOST: str = "127.0.0.1"
+    REDIS_HOST: str = "redis"
 
     JWT_SECRET: str = os.getenv("JWT_SECRET")
     JWT_ALGORITM: str = "HS256"
